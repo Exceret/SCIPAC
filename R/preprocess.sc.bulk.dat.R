5b4dfbf276ea1fbd97d689f15cb5e40789eca459
mianaz <zzeng4@nd.edu>
2025-10-14 16:24:28 -0400

    preliminary spatial support

--- R/preprocess.sc.bulk.dat.R
+++ R/preprocess.sc.bulk.dat.R
@@ -52,3 +52,117 @@ preprocess.sc.bulk.dat <- function(sc.dat, bulk.dat, hvg = 1000){
   return(list("sc.dat.preprocessed" = sc.dat.preprocessed,
               "bulk.dat.preprocessed" = bulk.dat.preprocessed))
 }
+
+
+#' Normalize spatial data and find highly variable genes using Seurat
+#'
+#' @param spatial.obj Seurat spatial object (e.g., from Load10X_Spatial)
+#' @param hvg the number of highly variable genes. The default is 1000.
+#' @param assay the assay to use. If NULL, will auto-detect from available assays.
+#' Common values: "Spatial" (Visium), "SCT" (spatial SCTransform), "RNA" (regular spatial)
+#'
+#' @return a spatial expression matrix. Rows stand for genes and columns stand for spots/cells.
+#' @importFrom dplyr %>%
+obtain.preprocessed.spatial.data <- function(spatial.obj, hvg = 1000, assay = NULL){
+
+  # Auto-detect assay if not specified
+  if(is.null(assay)){
+    available.assays <- Seurat::Assays(spatial.obj)
+    if("Spatial" %in% available.assays){
+      assay <- "Spatial"
+    } else if("SCT" %in% available.assays){
+      assay <- "SCT"
+    } else if("RNA" %in% available.assays){
+      assay <- "RNA"
+    } else {
+      stop("Could not auto-detect appropriate assay. Please specify assay parameter.")
+    }
+    message(paste0("Using assay: ", assay))
+  }
+
+  # Set default assay
+  Seurat::DefaultAssay(spatial.obj) <- assay
+
+  # Normalize and find variable features
+  # For Spatial assay, use standard normalization
+  if(assay %in% c("Spatial", "RNA")){
+    spatial.obj <- Seurat::NormalizeData(spatial.obj, normalization.method = "LogNormalize", scale.factor = 10000)
+    spatial.obj <- Seurat::FindVariableFeatures(spatial.obj, selection.method = "vst", nfeatures = hvg)
+
+    # Extract normalized data for variable genes
+    spatial.data.norm <- Seurat::GetAssayData(spatial.obj, assay = assay, layer = "data")
+    variable.genes <- Seurat::VariableFeatures(spatial.obj)
+
+  } else if(assay == "SCT"){
+    # For SCT assay, normalization is already done
+    variable.genes <- Seurat::VariableFeatures(spatial.obj)
+    if(length(variable.genes) < hvg){
+      warning(paste0("SCT assay only has ", length(variable.genes),
+                     " variable features. Using all available."))
+    }
+    spatial.data.norm <- Seurat::GetAssayData(spatial.obj, assay = "SCT", layer = "data")
+  }
+
+  processed.data <- spatial.data.norm[variable.genes, ] %>%
+    as.matrix()
+
+  return(processed.data)
+}
+
+
+#' Pre-process both spatial transcriptomics and bulk data
+#'
+#' This function first finds overlap genes between spatial and bulk data,
+#' then normalizes spatial data and finds variable genes,
+#' log-transforms bulk RNA-seq data,
+#' and uses high variable genes for both spatial and bulk data
+#'
+#' @param spatial.obj Seurat spatial object (e.g., from Load10X_Spatial)
+#' @param bulk.dat bulk data matrix. Rows stand for genes and columns stand for samples.
+#' @param hvg the number of highly variable genes. The default is \code{1000}.
+#' @param assay the assay to use from spatial object. If NULL, will auto-detect.
+#'
+#' @return A list object contains pre-processed spatial and bulk data
+#' @importFrom dplyr %>%
+#' @export
+#'
+preprocess.spatial.bulk.dat <- function(spatial.obj, bulk.dat, hvg = 1000, assay = NULL){
+
+  # Extract gene names from spatial object
+  if(is.null(assay)){
+    available.assays <- Seurat::Assays(spatial.obj)
+    if("Spatial" %in% available.assays){
+      assay <- "Spatial"
+    } else if("SCT" %in% available.assays){
+      assay <- "SCT"
+    } else if("RNA" %in% available.assays){
+      assay <- "RNA"
+    }
+  }
+
+  spatial.genes <- rownames(spatial.obj[[assay]])
+  overlap.genes <- intersect(spatial.genes, rownames(bulk.dat))
+
+  message(paste0("Found ", length(overlap.genes), " overlapping genes between spatial and bulk data"))
+
+  # Subset spatial object to overlap genes
+  spatial.obj.sub <- spatial.obj[overlap.genes, ]
+
+  # Subset and log-transform bulk data
+  bulk.dat.new <- bulk.dat[overlap.genes, ] %>% as.matrix()
+  bulk.dat.new <- log(bulk.dat.new + 1)
+
+  # Preprocess spatial data
+  spatial.dat.preprocessed <- obtain.preprocessed.spatial.data(
+    spatial.obj = spatial.obj.sub,
+    hvg = hvg,
+    assay = assay
+  )
+
+  # Use same genes for bulk data
+  bulk.dat.preprocessed <- bulk.dat.new[rownames(spatial.dat.preprocessed), ]
+
+  return(list("spatial.dat.preprocessed" = spatial.dat.preprocessed,
+              "bulk.dat.preprocessed" = bulk.dat.preprocessed,
+              "spatial.obj" = spatial.obj))  # Return original object for downstream use
+}
--- README.md
+++ README.md
@@ -4,7 +4,10 @@
 ## Introduction
 Numerous algorithms have been proposed to identify cell types in single-cell RNA sequencing data, yet a fundamental problem remains: determining associations between cells and phenotypes such as cancer. We develop SCIPAC (Single- Cell and bulk data-based Identifier for Phenotype Associated Cells), the first algorithm that quantitatively estimates the association between each cell in single-cell data and a phenotype. SCIPAC also provides a p-value for each association and applies to data with virtually any type of phenotype, for instance, binary (e.g., cancer vs. normal), ordinal (e.g., different stages of cancer), continuous (e.g., quantitative traits), or survival. SCIPAC also requires minimum tuning and is computationally very fast.
 
+**NEW**: SCIPAC now supports spatial transcriptomics data (Visium, Visium HD, Slide-seq, Xenium) while maintaining full backward compatibility with scRNA-seq workflows.
+
 ## Updates
+* Oct, 2024: Added spatial transcriptomics support (Visium, Visium HD, Slide-seq, Xenium).
 * Jun, 2024: minor updates in the package due to the changes in the dependencies.
 * Nov, 2022: SCIPAC version 1.0.0 is launched.
 
@@ -16,6 +19,33 @@ Numerous algorithms have been proposed to identify cell types in single-cell RNA
 Please see [http://RavenGan.github.io/SCIPAC/vignettes/introduction.html](http://RavenGan.github.io/SCIPAC/vignettes/introduction.html) for details. In the introduction, we also include how to use TCGA databases as the source of bulk RNA-seq data for the application of SCIPAC. In the R terminal, users can also use `?SCIPAC` to access the help documents.
 
 The code to generate the simulated data from three schemes is provided in the repository [SCIPAC_simulation](https://github.com/RavenGan/SCIPAC_simulation).
+
+### Using SCIPAC with Spatial Transcriptomics Data
+
+For spatial data (Visium, Visium HD, Slide-seq, Xenium), use the `preprocess.spatial.bulk.dat()` function instead of `preprocess.sc.bulk.dat()`:
+
+```r
+library(Seurat)
+library(SCIPAC)
+
+# Load spatial data
+spatial_obj <- Load10X_Spatial(data.dir = "path/to/spaceranger/output")
+
+# Preprocess spatial and bulk data
+spatial.bulk.prep <- preprocess.spatial.bulk.dat(spatial_obj, bulk.dat, hvg = 1000)
+
+# The rest of the pipeline is identical to scRNA-seq
+pca.res <- sc.bulk.pca(spatial.bulk.prep$spatial.dat.preprocessed,
+                       spatial.bulk.prep$bulk.dat.preprocessed, n.pc = 60)
+ct.res <- seurat.ct(pca.res$sc.dat.rot, res = 2.0)
+SCIPAC.res <- SCIPAC(pca.res$bulk.dat.rot, y, family = "binomial", ct.res = ct.res)
+
+# Combine results with spatial coordinates for visualization
+coords <- GetTissueCoordinates(spatial_obj)
+spatial.results <- cbind(coords, SCIPAC.res)
+```
+
+For more details, see `?preprocess.spatial.bulk.dat`.
 
 ## Examples
 In the [SCIPAC tutorial](http://RavenGan.github.io/SCIPAC/vignettes/introduction.html), we use multiple examples to show the function and performance of SCIPAC. Example datasets are also included in SCIPAC package.
